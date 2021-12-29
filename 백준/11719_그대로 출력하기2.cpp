1d95469332a36db61982539d76803c334480704d
jueun <57864944+dlwndms0812@users.noreply.github.com>
2021-12-29 22:40:18 +0900

    Create 11719_그대로 출력하기2.cpp

---
+++ 백준/11719_그대로 출력하기2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	while (getline(cin,str)) {
+		cout << str << endl;
+	}
+	return 0;
+}
+
+//11718 그대로 출력하기와 같은 코드
