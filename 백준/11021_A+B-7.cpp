391e0e978e29c2dbfd78b7373db4881fef38d32a
jueun <57864944+dlwndms0812@users.noreply.github.com>
2021-12-29 22:30:57 +0900

    Create 11021_A+B-7.cpp

---
+++ 백준/11021_A+B-7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a + b << endl;
+	}
+
+	return 0;
+}
