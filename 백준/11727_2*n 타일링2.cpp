c3fcf9d09807c72ba39a9701d4a7a69083f2d5b5
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-01-04 23:47:34 +0900

    Create 11727_2*n 타일링2.cpp

---
+++ 백준/11727_2*n 타일링2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int dp[1001] = { 0 };
+
+void fun(int n) {
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + 2 * dp[i - 2];
+		dp[i] = dp[i] % 10007;
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 3;
+	if (n > 2) {
+		fun(n);
+	}
+	cout << dp[n];
+	return 0;
+}
