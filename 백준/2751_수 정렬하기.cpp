bd8d268fd650c18675c9ce4841ce976e77232b8c
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-01-11 02:06:53 +0900

    Create 2751_수 정렬하기.cpp

---
+++ 백준/2751_수 정렬하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	static int arr[1000001];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+	
+	for (int i = 0; i < n; i++)
+		printf("%d\n", arr[i]);
+
+	return 0;
+}
