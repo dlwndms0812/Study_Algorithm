0022111a94fbdf979f1685db242231c0c1744cd9
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-01-17 23:55:07 +0900

    Create 10809_알파벳 찾기.cpp

---
+++ 백준/10809_알파벳 찾기.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int arr[26];
+    for (int i = 0; i < 26; i++) {
+        arr[i] = -1;
+    }
+
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.size(); i++) {
+        if (arr[str[i] - 'a'] == -1) {
+            arr[str[i] - 'a'] = i;
+        }
+
+    }
+
+    for (int i = 0; i < 26; i++) {
+        cout << arr[i] << endl;
+    }
+    return 0;
+	return 0;
+}
