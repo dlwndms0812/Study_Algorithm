62836fcdea9711036091faf4f00cba56076d8b7b
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-02-02 03:29:40 +0900

    Create 15596_정수 N개의 합.cpp

---
+++ 백준/15596_정수 N개의 합.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long sum(std::vector<int>& a) {
+    long long res = 0;
+    for (int i = 0; i < a.size(); i++) {
+        res += a[i];
+    }
+    return res;
+}
