b26314b8838d9a5cfe538b2e88440ec306816fa9
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-03-21 18:44:50 +0900

    Create 11279_최대 힙.cpp

---
+++ 백준/11279_최대 힙.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+
+    priority_queue<int>pq;
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+
+        if (k == 0) {
+            if (pq.empty())
+                printf("0\n");
+            else {
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+        }
+        else
+            pq.push(k);
+    }
+    return 0;
+
+}
+   
