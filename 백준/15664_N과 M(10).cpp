c4a914d97dbdeae1f3b8c19a1cce716ae51b22c4
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-04-01 17:21:54 +0900

    Create 15664_N과 M(10).cpp

---
+++ 백준/15664_N과 M(10).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+int arr[9];
+int arr1[9];
+bool visited[9];
+
+void dfs(int cnt, int k) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    
+    int check = 0;
+    for (int i = k; i < n; i++) {
+        
+        if (!visited[i] && arr1[i]!= check) {
+            visited[i] = true;
+            arr[cnt] = arr1[i];
+            check = arr[cnt];
+            dfs(cnt + 1, i);
+            visited[i] = false;
+            
+        }
+        
+    }
+
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    sort(arr1, arr1 + n);
+
+    
+    dfs(0,0);
+
+    return 0;
+
+}
