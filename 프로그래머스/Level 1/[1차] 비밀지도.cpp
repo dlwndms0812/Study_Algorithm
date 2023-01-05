ad55960e7a26fd62f33d76cb7163d8d96e310bf8
jueun <57864944+dlwndms0812@users.noreply.github.com>
2023-01-05 21:00:06 +0900

    Update [1차] 비밀지도.cpp

--- 프로그래머스/Level 1/[1차] 비밀지도.cpp
+++ 프로그래머스/Level 1/[1차] 비밀지도.cpp
@@ -62,3 +62,29 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     }
     return answer;
 }
+
+//더 간단한 풀이
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    
+    for (int i = 0; i < n; i++)
+    {
+        string str;
+        arr1[i] = arr1[i] | arr2[i];
+        for (int j = 0; j < n; j++)
+        {
+            if (arr1[i] % 2 == 0)
+                str = ' ' + str;
+            else
+                str = '#' + str;
+            arr1[i] = arr1[i] >> 1;
+        }
+        answer.push_back(str);
+    }
+    return answer;
+}
