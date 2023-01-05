7743ae23743edbcf2a5ddcdda0d50fb5f4bf2c59
jueun <57864944+dlwndms0812@users.noreply.github.com>
2023-01-05 21:35:53 +0900

    Create 문자열 내 마음대로 정렬하기.cpp

---
+++ 프로그래머스/Level 1/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int num;
+
+bool cmp(string a, string b)
+{
+    if (a[num] == b[num])
+        return a < b;
+    else
+        return a[num] < b[num];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    num = n;
+    sort(strings.begin(), strings.end(), cmp);
+    
+    return strings;
+}
