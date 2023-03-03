91b2ce601b2c9dfb899d6811a99db8ad4a0a95d5
jueun <57864944+dlwndms0812@users.noreply.github.com>
2023-03-03 18:21:25 +0900

    Create 25314_코딩은 체육과목 입니다.cpp

---
+++ 백준/25314_코딩은 체육과목 입니다.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n != 4)
+    {
+        cout << "long ";
+        n = n - 4;
+    }
+    cout << "long int";
+
+    return 0;
+}
