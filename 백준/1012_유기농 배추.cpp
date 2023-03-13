289055d6baafecc67ed17f0e5e37e8128eb314f2
jueun <57864944+dlwndms0812@users.noreply.github.com>
2023-03-13 19:21:53 +0900

    Update 1012_유기농 배추.cpp

--- 백준/1012_유기농 배추.cpp
+++ 백준/1012_유기농 배추.cpp
@@ -1,3 +1,4 @@
+//dfs
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -60,4 +61,86 @@ int main() {
 
     return 0;
 }
-   
+  
+//bfs
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int t, n, m, k;
+int arr[51][51];
+int visited[51][51];
+
+int dy[4] = { 1, 0, -1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+
+
+void bfs(int y, int x)
+{
+    visited[y][x] = 1;
+    
+    queue<pair<int, int>> q;
+    q.push({y, x});
+    
+    while (!q.empty())
+    {
+        int yy = q.front().first;
+        int xx = q.front().second;
+        
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = yy + dy[i];
+            int nx = xx + dx[i];
+            
+            if (ny < 0 || nx < 0 || ny >= n || nx >= m)
+                continue;
+            if (arr[ny][nx] == 0 || visited[ny][nx] == 1)
+                continue;
+                
+            q.push({ny, nx});
+            visited[ny][nx] = 1;
+        }
+    }
+}
+
+int main()
+{
+    cin >> t;
+    
+    while (t--)
+    {
+        cin >> m >> n >> k;
+    
+        for (int i = 0; i < n; i++)
+        {
+            fill(arr[i], arr[i] + m, 0);
+            fill(visited[i], visited[i] + m, 0);
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            int x, y;
+            cin >> x >> y;
+            arr[y][x] = 1;
+        }
+    
+        int cnt = 0;
+    
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (arr[i][j] == 1 && visited[i][j] == 0)
+                {
+                    bfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+        printf("%d\n", cnt);
+    }
+    return 0;
+}
