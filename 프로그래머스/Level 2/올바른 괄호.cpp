074d70b43f26d2dba2f52f3f0d7c78007cffe1eb
jueun <57864944+dlwndms0812@users.noreply.github.com>
2023-12-27 16:58:09 +0900

    Create 올바른 괄호.cpp

---
+++ 프로그래머스/Level 2/올바른 괄호.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    stack<char> st;
+
+    for (char c : s) {
+        if (c == '(')
+            st.push('(');
+        else
+        {
+            if (st.empty()) {
+                answer = false;
+                break ;
+            }
+            else if (st.top() == ')')
+            {
+                answer = false;
+                break ;
+            }
+            else
+                st.pop();
+        }
+    }
+
+    if (!st.empty())
+        answer = false;
+    
+    return answer;
+}
