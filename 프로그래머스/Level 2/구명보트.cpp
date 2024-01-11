723058775a409d0da932abbdedcbb64fd92ec89f
jueun <57864944+dlwndms0812@users.noreply.github.com>
2024-01-11 20:48:27 +0900

    Create 구명보트.cpp

---
+++ 프로그래머스/Level 2/구명보트.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    
+    int start = 0;
+    int end = people.size() - 1;
+    
+    sort(people.begin(), people.end());
+    
+    while (start < end) {
+        if ((people[start] + people[end]) > limit) {
+            end--;
+            answer++;
+        }
+        else if ((people[start] + people[end]) == limit) {
+            start++;
+            end--;
+            answer++;
+        }
+        else {
+            start++;
+            end--;
+            answer++;
+        }
+    }
+    
+    if (start == end)
+        answer++;
+    
+    return answer;
+}
