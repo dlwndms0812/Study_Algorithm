57f4ecee26e8b913d0e7c32286900d91f497959a
jueun <57864944+dlwndms0812@users.noreply.github.com>
2024-03-09 20:56:42 +0900

    Update 이중우선순위큐.cpp

--- 프로그래머스/Level 3/이중우선순위큐.cpp
+++ 프로그래머스/Level 3/이중우선순위큐.cpp
@@ -1,3 +1,99 @@
+//priority_queue를 사용한 방법
+
+#include <string>
+#include <vector>
+#include <queue>
+
+
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    
+    priority_queue<int, vector<int>, greater<int>> q_min;
+    priority_queue<int, vector<int>, less<int>> q_max;
+    int queue_size = 0;
+    
+    for (int i = 0; i < operations.size(); i++) {
+        string str = operations[i];
+        
+        if (str[0] == 'I') {
+            string value = str.substr(2, str.size());
+            int num = stoi(value);
+            
+            q_min.push(num);
+            q_max.push(num);
+            queue_size++;
+        
+        } else if (str[0] == 'D') {
+            if (queue_size == 0)
+                continue;
+            
+            if (str[2] == '1')
+                q_max.pop();
+            else
+                q_min.pop();
+            
+            queue_size--;
+            
+            if (queue_size == 0) {
+                while (!q_min.empty())
+                    q_min.pop();
+                
+                while (!q_max.empty())
+                    q_max.pop();
+            
+            }
+        }   
+    }
+    
+    if (queue_size == 0) {
+        while (!q_min.empty())
+                q_min.pop();
+                
+        while (!q_max.empty())
+                q_max.pop();
+    }
+            
+    
+    
+    if (queue_size == 0) {
+        answer.push_back(0);
+        answer.push_back(0);
+    }
+    else if (queue_size == 1) {
+        int num;
+        
+        if (q_min.empty())
+            num = q_max.top();
+        else
+            num = q_min.top();
+        
+        if (num < 0) {
+            answer.push_back(0);
+            answer.push_back(num);
+        }
+        else {
+            answer.push_back(num);
+            answer.push_back(0);
+        }
+    }
+    else {
+        answer.push_back(q_max.top());
+        answer.push_back(q_min.top());
+    }
+    
+    return answer;
+}
+
+
+
+
+
+
+//vector를 사용한 방법
+
+
 #include <string>
 #include <vector>
 #include <algorithm>
